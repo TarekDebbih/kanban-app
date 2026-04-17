c322f6ffe8cde4d99d640dd081f1a7792fae0a5f
DEBBIH <tarek.debbih@etu.unistra.fr>
2026-04-17 17:45:41 +0200

    update Ticket model with validation annotatios and new required field

--- backend/KanbanApi/Models/Ticket.cs
+++ backend/KanbanApi/Models/Ticket.cs
@@ -1,16 +1,22 @@
-namespace KanbanApi.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace KanbanApi.Models;
+
+public class Ticket
 {
-    public class Ticket
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public int Position { get; set; }
+    public int Id { get; set; }
 
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+    [Required]
+    public string Title { get; set; } = string.Empty;
 
-        public int KanbanColumnId { get; set; }
-        public KanbanColumn? KanbanColumn { get; set; }
-    }
+    public string Description { get; set; } = string.Empty;
+
+    public decimal TimeSpentHours { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public int Position { get; set; }
+
+    public int KanbanColumnId { get; set; }
+    public KanbanColumn? KanbanColumn { get; set; }
 }
