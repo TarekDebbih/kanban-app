848c9dbc9e441c84974ccd98633f6853cd2218e7
DEBBIH <tarek.debbih@etu.unistra.fr>
2026-04-17 11:33:19 +0200

    (fix) add methodes to user service interface

--- backend/KanbanApi/Services/IUserService.cs
+++ backend/KanbanApi/Services/IUserService.cs
@@ -4,6 +4,8 @@ namespace KanbanApi.Services;
 
 public interface IUserService
 {
-    Task<User?> GetUserByEmailAsync(string email);
+    Task<User?> GetByIdAsync(int id);
+    Task<User?> GetByEmailAsync(string email);
+    Task<List<User>> GetAllAsync();
     Task<User> CreateUserAsync(User user);
 }
